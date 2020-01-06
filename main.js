b757350253cb858a3c8ac0e4503e25934c24e6af
Spencer Fisher <Fisher.spencer@gmail.com>
2020-01-06 14:39:12 -0800

    cookies

--- main.js
+++ main.js
@@ -1,19 +1,60 @@
-var request = new XMLHttpRequest()
+function setCookie(cname,cvalue,exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = ";expires=" + d;
+    document.cookie = cname + "=" + cvalue + expires + ";samesite=none;domain=.github.io"+";path=/";
+}
 
-// todo pull cookie if exists only query if cookie is older than 1 hr
-var url = 'https://www.aavso.org/vsx/index.php?view=api.delim&ident=Betelgeuse&fromjd=2458124&tojd=2458125&delimiter=@@@'
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
 
-request.open('GET', url, true)
-request.onload = function() {
-  // Begin accessing JSON data here
-  var data = this.response
-
-  if (request.status >= 200 && request.status < 400) {
-  	console.log(data)
-  	// TODO store cookie
+function checkCookie() {
+  var user=getCookie("username");
+  if (user != "") {
+    alert("Welcome again " + user);
   } else {
-    console.log('error')
+     user = prompt("Please enter your name:","");
+     if (user != "" && user != null) {
+       setCookie("username", user, 30);
+     }
   }
 }
 
-request.send()
+function getRequest() {
+    var request = new XMLHttpRequest()
+
+    // todo pull cookie if exists only query if cookie is older than 1 hr
+    var url = 'https://www.aavso.org/vsx/index.php?view=api.delim&ident=Betelgeuse&fromjd=2458124&tojd=2458125&delimiter=@@@'
+
+    request.open('GET', url, true)
+    request.onload = function() {
+        // Begin accessing JSON data here
+        var data = this.response
+
+        if (request.status >= 200 && request.status < 400) {
+            console.log(data)
+            return data
+            // TODO store cookie
+        } else {
+            console.log('error')
+        }
+    }
+
+    request.send()
+}
+
+//checkCookie();
+setCookie("username", "user", 30);
