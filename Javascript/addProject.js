45c9d3d6003b5f8b148c73699379023c41bbed17
Spencer Fisher <spencer.fisher@billgo.com>
2024-03-17 11:03:20 -0700

    redirects

--- Javascript/addProject.js
+++ Javascript/addProject.js
@@ -1,22 +1,17 @@
 function OnLoad() {
-    if(navigator.userAgent.toLowerCase().indexOf("iphone") > -1){ window.location.href = 'https://itunes.apple.com/my/app/flipbizz/idexampleapp'; }
+    if(navigator.userAgent.toLowerCase().indexOf("iphone") > -1){
+		//window.location.href = 'https://itunes.apple.com/my/app/flipbizz/idexampleapp';
+		window.location.href = 'https://play.google.com/store/apps/details?id=com.goat.project_pics';
+		}
 
-    if(navigator.userAgent.toLowerCase().indexOf("android") > -1){ window.location.href = 'https://play.google.com/store/apps/details?id=com.goat.project_pics'; }
-
-    //Update #2
-    if (!navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/)) {
-         window.location.href = 'https://www.google.com'; //Desktop Browser
-    }
-}
-
-//dummychange
-function OnLoadTwo() {
-    if(navigator.userAgent.toLowerCase().indexOf("iphone") > -1){ window.location.href = 'https://itunes.apple.com/my/app/flipbizz/idexampleapp'; }
-
-    if(navigator.userAgent.toLowerCase().indexOf("android") > -1){ window.location.href = 'https://play.google.com/store/apps/details?id=com.goat.project_pics'; }
+    if(navigator.userAgent.toLowerCase().indexOf("android") > -1){
+		window.location.href = 'https://play.google.com/store/apps/details?id=com.goat.project_pics';
+		}
 
     //Update #2
     if (!navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/)) {
-         window.location.href = 'https://www.google.com'; //Desktop Browser
+		// we should flesh out the page a bit
+		// for now also redirect to the app page
+		window.location.href = 'https://play.google.com/store/apps/details?id=com.goat.project_pics';
     }
 }
