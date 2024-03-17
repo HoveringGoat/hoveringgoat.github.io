4f1915017487f746338eae660b6d09aab18d1c1a
Spencer Fisher <spencer.fisher@billgo.com>
2024-03-17 10:53:37 -0700

    onload

--- Javascript/addProject.js
+++ Javascript/addProject.js
@@ -1,4 +1,4 @@
-function myFunction() {
+function OnLoad() {
     if(navigator.userAgent.toLowerCase().indexOf("iphone") > -1){ window.location.href = 'https://itunes.apple.com/my/app/flipbizz/idexampleapp'; }
 
     if(navigator.userAgent.toLowerCase().indexOf("android") > -1){ window.location.href = 'https://play.google.com/store/apps/details?id=com.goat.project_pics'; }
