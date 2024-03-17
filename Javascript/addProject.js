ae15843bdfc1f1efbe829d36583d9d329e3d2216
Spencer Fisher <spencer.fisher@billgo.com>
2024-03-17 11:13:03 -0700

    Update addProject.js

--- Javascript/addProject.js
+++ Javascript/addProject.js
@@ -1,17 +1,17 @@
 function OnLoad() {
     if(navigator.userAgent.toLowerCase().indexOf("iphone") > -1){
 		//window.location.href = 'https://itunes.apple.com/my/app/flipbizz/idexampleapp';
-		window.location.href = 'https://play.google.com/store/apps/details?id=com.goat.project_pics';
+		window.location.href = 'https://play.google.com/apps/testing/com.goat.project_pics';
 		}
 
     if(navigator.userAgent.toLowerCase().indexOf("android") > -1){
-		window.location.href = 'https://play.google.com/store/apps/details?id=com.goat.project_pics';
+		window.location.href = 'https://play.google.com/apps/testing/com.goat.project_pics';
 		}
 
     //Update #2
     if (!navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/)) {
 		// we should flesh out the page a bit
 		// for now also redirect to the app page
-		window.location.href = 'https://play.google.com/store/apps/details?id=com.goat.project_pics';
+		window.location.href = 'https://play.google.com/apps/testing/com.goat.project_pics';
     }
 }
